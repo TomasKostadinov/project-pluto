c6218b4e0780bd96162e64085f9e779c9f331e68
Tomas Kostadinov <github@kostadinov.it>
2022-09-23 23:15:20 +0200

    * Meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,19 @@ const Home: NextPage = () => {
     return (
         <div className="">
             <Head>
-                <title>Vaerk Project Pluto</title>
-                <meta name="description" content="AI created vision"/>
+                <title>AI Created Vision Statement for 2035 - Vaerk Project Pluto</title>
+                <meta name="description" content="For us humans sometimes it is hard to imagine the future. Project pluto enables you to get aboard a rocket to 2035 and find your company's sustainable vision!"/>
                 <link rel="icon" href="/x.svg"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://project-pluto.vaerk.digital/"/>
+                <meta property="og:title" content="Home - vaerk"/>
+                <meta property="og:description" content=""/>
+                <meta property="og:image" content="https://project-pluto.vaerk.digital/bg.jpg"/>
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://project-pluto.vaerk.digital/"/>
+                <meta property="twitter:title" content="AI Created Vision Statement for 2035 - Vaerk Project Pluto"/>
+                <meta property="twitter:description" content="For us humans sometimes it is hard to imagine the future. Project pluto enables you to get aboard a rocket to 2035 and find your company's sustainable vision!"/>
+                <meta property="twitter:image" content="https://project-pluto.vaerk.digital/bg.jpg"/>
             </Head>
             <main>
                 <Vision></Vision>
