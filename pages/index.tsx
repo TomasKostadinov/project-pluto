9dee3fc8b3df4f8447d8b2b1b00b6288e2c391fe
Janick Oswald <janick@vaerk.digital>
2022-09-24 00:59:41 +0200

    adjusted Vaerk to vaerk in index-tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,12 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/x.svg"/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:url" content="https://project-pluto.vaerk.digital/"/>
-                <meta property="og:title" content="AI Created Vision Statement for 2035 - Vaerk Project Pluto"/>
+                <meta property="og:title" content="AI Created Vision Statement for 2035 - vaerk Project Pluto"/>
                 <meta property="og:description" content=""/>
                 <meta property="og:image" content="https://project-pluto.vaerk.digital/bg.jpg"/>
                 <meta property="twitter:card" content="summary_large_image"/>
                 <meta property="twitter:url" content="https://project-pluto.vaerk.digital/"/>
-                <meta property="twitter:title" content="AI Created Vision Statement for 2035 - Vaerk Project Pluto"/>
+                <meta property="twitter:title" content="AI Created Vision Statement for 2035 - vaerk Project Pluto"/>
                 <meta property="twitter:description" content="For us humans sometimes it is hard to imagine the future. Project pluto enables you to get aboard a rocket to 2035 and find your company's sustainable vision!"/>
                 <meta property="twitter:image" content="https://project-pluto.vaerk.digital/bg.jpg"/>
             </Head>
